187efe3f202d6c746d211e1b13d5c2d34b800cbb
armaan-crypto <armaanahmed08@gmail.com>
2023-03-04 11:11:10 -0500

    Initial Commit

---
+++ HackTJ/HackTJApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackTJApp.swift
+//  HackTJ
+//
+//  Created by Armaan Ahmed on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct HackTJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
